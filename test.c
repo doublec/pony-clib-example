e2355cceafa25ca3d491c10493f68f44da7a2353
Chris Double <chris.double@double.co.nz>
2017-08-01 14:22:20 +1200

    Fix bitrot, bring up to date with Pony 0.16.1

--- test.c
+++ test.c
@@ -7,15 +7,16 @@ int main(int argc, char** argv) {
   pony_init(argc, argv);
 
   Foo* x = Foo_Alloc();
-  Foo_create__send(x);
-  Foo_hi__send(x);
+  Foo_tag_create_o__send(x);
+  Foo_tag_hi_o__send(x);
 
-  pony_start(1);
-//  pony_register_thread();
+  if (pony_start(true, true) == -1) {
+    printf("Failed to start Pony runtime\n");
+  }
 
   Foo* y = Foo_Alloc();
-  Foo_create__send(y);
-  Foo_hi__send(y);
+  Foo_tag_create_o__send(y);
+  Foo_tag_hi_o__send(y);
   pony_stop();
   return 0;
 }
